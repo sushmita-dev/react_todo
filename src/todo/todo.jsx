556c27d142eff32da5c04cdff2a5e215934a8114
sushmita-dev <sdey7208@gmail.com>
2024-01-24 16:06:35 +0530

    todo is being added

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import bin from '../assests/images/trash-can.png'
+import './todo.css'
 
 function Todo() {
     const [todoText, setTodoText] = useState('')
@@ -10,24 +12,38 @@ function Todo() {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        if (todoText.trim() === '') {
+            return;
+        }
+
         setTodoList([...todolist, todoText])
         setTodoText('')
     }
     return (
-        <div>
-            <h3>Sushmita's Todos</h3>
-            <div>
-                <input value={todoText} type='text' onChange={e => handleChange(e)} />
-                <button onClick={e => handleSubmit(e)} > Submit</button>
+        <div className='todo'>
+            <div className='todo-header'>
+                <h3>Sushmita's Todos</h3>
             </div>
-            <div>
-                <ul>
-                    {
-                        todolist.map((todo) => {
-                            return <li>{todo}</li>
-                        })
-                    }
-                </ul>
+            <form className='todo-input' onSubmit={e => handleSubmit()}>
+                <input placeholder="Add some todos..." value={todoText} type='text' onChange={e => handleChange(e)} className='todo-text-input' />
+                <button onClick={e => handleSubmit(e)} className='todo-button'> Submit</button>
+            </form>
+            <div className='todo-body'>
+                {
+                    todolist.length === 0 ?
+
+                        <span className='empty-todo'>
+                            Your Todo is empty
+                        </span>
+                        : <div className='todo-list'>
+                            {
+                                todolist.map((todo) => {
+                                    return <div className='todo-element'>{todo}<img className="delete-button"width='20px' height='20px' src={bin} alt="delete icon" /> </div>
+                                })
+                            }
+                        </div>
+
+                }
             </div>
         </div>
     )
